2f7726f459072e7d46dd1c05b7b3bd1b13c43e81
Teekay1702 <tokoloho57@gmail.com>
2025-04-02 20:25:34 +0200

    Add project files.

---
+++ wwwroot/js/signalr-setup.js
@@ -0,0 +1,7 @@
+﻿const connection = new signalR.HubConnectionBuilder()
+    .withUrl("/chatHub")  // Replace "/chatHub" with your actual hub URL if different
+    .build();
+
+connection.start()
+    .then(() => console.log("Connected to SignalR"))
+    .catch(err => console.error("Error connecting:", err));
